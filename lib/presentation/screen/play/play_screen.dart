f5a720e149d6df035995f892f08da616880a2c75
Muhammadali <muhammadalinomonov837@gmail.com>
2023-08-25 17:09:00 +0500

    second commit

--- lib/presentation/screen/play/play_screen.dart
+++ lib/presentation/screen/play/play_screen.dart
@@ -34,89 +34,88 @@ class _PlayScreenState extends State<PlayScreen> {
       ),
     );
     return YoutubePlayerBuilder(
-            player: YoutubePlayer(
-              controller: controller,
-              progressColors: ProgressBarColors(
-                  backgroundColor: Colors.grey,
-                  playedColor: Colors.red,
-                  handleColor: Colors.red.shade400),
-              showVideoProgressIndicator: true,
-            ),
-            builder: (context, player) {
-              return BlocProvider.value(
-                value: block,
-                child: BlocBuilder<PlayBloc, PlayState>(
-                  builder: (context, state) {
-                    return Scaffold(
-                      body: SafeArea(
-                        child: Column(
-                          children: [
-                            player,
-                            Container(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 12, vertical: 8),
-                              child: Text(widget.video.title ?? "",
-                                  style: const TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w600)),
-                            ),
-                            Container(
-                                margin:
-                                    const EdgeInsets.symmetric(horizontal: 12),
-                                alignment: Alignment.centerRight,
-                                child: Text(
-                                  "${getTimeAgo(widget.video.uploadDate ?? "")} ago",
-                                  textAlign: TextAlign.end,
-                                  style: const TextStyle(
-                                      color: Colors.grey,
-                                      fontWeight: FontWeight.w300,
-                                      fontSize: 14),
-                                )),
-                            Container(
-                                margin: const EdgeInsets.only(left: 12),
-                                alignment: Alignment.centerLeft,
-                                child: const Text(
-                                  "Recommended for you",
-                                  style: TextStyle(
-                                      fontSize: 22,
-                                      fontWeight: FontWeight.w600,
-                                      color: Colors.red),
-                                )),
-                            const SizedBox(height: 8),
-                            if (state.status == EnumStatus.loading)
-                              const ShimmerComponent()
-                            else if (state.status == EnumStatus.success)
-                              Expanded(
-                                child: SizedBox(
-                                  width: double.infinity,
-                                  child: ListView.builder(
-                                      shrinkWrap: true,
-                                      scrollDirection: Axis.vertical,
-                                      itemCount: state.videosList.length,
-                                      itemBuilder: (_, i) {
-                                        return VideoItem(
-                                          lessonData: state.videosList[i],
-                                          onClick: (videoData) {
-                                            block.add(ChangeVideo(
-                                                videoData: state.videosList[i],
-                                                context: context,
-                                                status: widget.status));
-                                          },
-                                        );
-                                      }),
-                                ),
-                              )
-                          ],
-                        ),
+      player: YoutubePlayer(
+        controller: controller,
+        progressColors: ProgressBarColors(
+            backgroundColor: Colors.grey,
+            playedColor: Colors.red,
+            handleColor: Colors.red.shade400),
+        showVideoProgressIndicator: true,
+      ),
+      builder: (context, player) {
+        return BlocProvider.value(
+          value: block,
+          child: BlocBuilder<PlayBloc, PlayState>(
+            builder: (context, state) {
+              return Scaffold(
+                body: SafeArea(
+                  child: Column(
+                    children: [
+                      player,
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 12, vertical: 8),
+                        child: Text(widget.video.title ?? "",
+                            style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600)),
                       ),
-                    );
-                  },
+                      Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 12),
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            "${getTimeAgo(widget.video.uploadDate ?? "")} ago",
+                            textAlign: TextAlign.end,
+                            style: const TextStyle(
+                                color: Colors.grey,
+                                fontWeight: FontWeight.w300,
+                                fontSize: 14),
+                          )),
+                      Container(
+                          margin: const EdgeInsets.only(left: 12),
+                          alignment: Alignment.centerLeft,
+                          child: const Text(
+                            "Recommended for you",
+                            style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.red),
+                          )),
+                      const SizedBox(height: 8),
+                      if (state.status == EnumStatus.loading)
+                        const ShimmerComponent()
+                      else if (state.status == EnumStatus.success)
+                        Expanded(
+                          child: SizedBox(
+                            width: double.infinity,
+                            child: ListView.builder(
+                                shrinkWrap: true,
+                                scrollDirection: Axis.vertical,
+                                itemCount: state.videosList.length,
+                                itemBuilder: (_, i) {
+                                  return VideoItem(
+                                    lessonData: state.videosList[i],
+                                    onClick: (videoData) {
+                                      block.add(ChangeVideo(
+                                          videoData: state.videosList[i],
+                                          context: context,
+                                          status: widget.status));
+                                    },
+                                  );
+                                }),
+                          ),
+                        )
+                    ],
+                  ),
                 ),
               );
             },
-          );
-       /* },
+          ),
+        );
+      },
+    );
+    /* },
       ),
     );*/
   }
